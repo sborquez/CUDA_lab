4344bd053e7c3f8db9fc455c471a8bc3813a4103
Sebastian Borquez <sebastian.borquez.g@gmail.com>
2020-01-17 21:47:29 -0300

    pregunta  2.b y 2.c

--- lab1/pregunta2.cu
+++ lab1/pregunta2.cu
@@ -17,24 +17,23 @@ void init(float* &y_ji, float* &dev_y_ji, int m) {
 
 // Pregunta 2.a
 void update_serial(float* y_ji, float dt, float t_i, int m) {
-    float t_ip = t_i - dt;
+    float t_ip = t_i - dt;  //t_{i-1}
     for(int j=0; j<m; ++j) {
         y_ji[j] += dt*(4*t_ip - y_ji[j] + 3 + j);
     }
 }
-
-/*
-// Pregunta 1.b
-__global__ void euler_paralelo(float* y_t, float dt, int n) {
+// Pregunta 2.b
+__global__ void update_paralelo(float* dev_y_ji, float dt, float t_i, int m) {
     int Tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (Tid <= n) {
-        y_t[Tid] = 0;
-        for (int j=0; j < Tid; ++j) {
-            y_t[Tid] += 9*powf(j*dt, 2.0) - 4*j*dt + 5;
-        }
-        y_t[Tid] = 4 + y_t[Tid]*dt;
+    if (Tid < m) {
+        float t_ip = t_i - dt; //t_{i-1}
+        dev_y_ji[Tid] += dt*(4*t_ip - dev_y_ji[Tid] + 3 + Tid);
     }
 }
+
+
+/*
+
 
 // Pregunta 1.c
 void euler_hibrida_sumatoria(float dt, int n, float* sumatoria) {
@@ -60,14 +59,12 @@ int main(int argc, int* argv)
     float t_i;
     
     float* y_ji;
-    // float* sumatoria;
     clock_t start, end;
     
     int block_size = BLOCK_SIZE;
     int grid_size;
     float* dev_y_ji;
-    // float* dev_sumatoria;
-    // float elapsed;
+    float elapsed;
 
     // Pregunta 2-a
     printf("Pregunta 2.a\n");
@@ -92,76 +89,67 @@ int main(int argc, int* argv)
         free(y_ji);
     }
 
-    // // Pregunta 1-b
-    // printf("Pregunta 1.b\n");
-    // for (int m=1; m<=4 ; m++) {
-    //     // Preparar variables
-    //     dt = powf(10.0, -1.0*m);
-    //     n = (int)powf(10, m+1);
-    //     grid_size = (int)ceil((float)(n+1)/block_size);
-
-    //     y_t = (float*)malloc(sizeof(float)*(n+1));
-    //     cudaMalloc(&dev_y_t, (n+1)*sizeof(float));
-
-    //     // Evaluacion
-    //     cudaEvent_t ct1, ct2;
-    //     cudaEventCreate(&ct1);
-    //     cudaEventCreate(&ct2);
-    //     cudaEventRecord(ct1);
-    //     euler_paralelo<<<grid_size, block_size>>>(dev_y_t, dt, n);
-    //     cudaEventRecord(ct2);
-    //     cudaEventSynchronize(ct2);
-    //     cudaEventElapsedTime(&elapsed, ct1, ct2);
-    //     cudaMemcpy(y_t, dev_y_t, (n+1)*sizeof(float), cudaMemcpyDeviceToHost);
-
-    //     //Mostrar resultado 
-    //     printf("delta_t=10^-%d n=%d elapsed=%f [ms]\n", m, n,elapsed);
+    // Pregunta 2-b
+    printf("Pregunta 2.b\n");
+    for (int m_=4; m_<=8; m_++) {
+        // Preparar variables
+        m = (int)powf(10.0, (float)m_);
+        t_i = 0.0;
+        grid_size = (int)ceil((float)(m)/block_size);
+        init(y_ji, dev_y_ji, m);
         
-    //     cudaFree(dev_y_t);
-    //     free(y_t);
-    // }
+        // Evaluacion
+        cudaEvent_t ct1, ct2;
+        cudaEventCreate(&ct1);
+        cudaEventCreate(&ct2);
+        cudaEventRecord(ct1);
+        for (int i=0; i<=n; ++i) {
+            update_paralelo<<<grid_size, block_size>>>(dev_y_ji, dt, t_i, m);
+            t_i += dt;
+        }
+        cudaEventRecord(ct2);
+        cudaEventSynchronize(ct2);
+        cudaEventElapsedTime(&elapsed, ct1, ct2);
+        cudaMemcpy(y_ji, dev_y_ji, (m)*sizeof(float), cudaMemcpyDeviceToHost);
 
-    // // Pregunta 1-c
-    // printf("Pregunta 1.c\n");
-    // for (int m=1; m<=6 ; m++) {
-    //     // Preparar variables
-    //     dt = powf(10.0, -1.0*m);
-    //     n = (int)powf(10, m+1);
-    //     grid_size = (int)ceil((float)(n+1)/block_size);
-
-    //     sumatoria = (float*)malloc(sizeof(float)*(n+1));
-    //     y_t = (float*)malloc(sizeof(float)*(n+1));
-
-    //     cudaMalloc(&dev_y_t, (n+1)*sizeof(float));
-    //     cudaMalloc(&dev_sumatoria, (n+1)*sizeof(float));
-
-    //     // Sumatoria
-    //     start = clock();
-    //     euler_hibrida_sumatoria(dt, n, sumatoria);
-
-    //     // Copiar valores
-    //     cudaMemcpy(dev_sumatoria, sumatoria, (n+1)*sizeof(float), cudaMemcpyHostToDevice);
-    //     end = clock();
-
-    //     // Evaluacion
-    //     cudaEvent_t ct1, ct2;
-    //     cudaEventCreate(&ct1);
-    //     cudaEventCreate(&ct2);
-    //     cudaEventRecord(ct1);
-    //     euler_hibrida_paralelo<<<grid_size, block_size>>>(dev_y_t, dt, n, dev_sumatoria);
-    //     cudaEventRecord(ct2);
-    //     cudaEventSynchronize(ct2);
-    //     cudaEventElapsedTime(&elapsed, ct1, ct2);
-
-    //     cudaMemcpy(y_t, dev_y_t, (n+1)*sizeof(float), cudaMemcpyDeviceToHost);
+        //Mostrar resultado 
+        printf("m=10^%d elapsed=%f [ms]\n", m_, elapsed);
         
-    //     //Mostrar resultado 
-    //     printf("delta_t=10^-%d n=%d elapsed=%f [ms]\n", m, n, elapsed + (double)((1000*(end - start))/CLOCKS_PER_SEC));
-        
-    //     free(y_t);
-    //     free(sumatoria);
+        // Liberar memoria
+        cudaFree(dev_y_ji);
+        free(y_ji);
+    }
 
-    //     cudaFree(dev_y_t);
-    //     cudaFree(dev_sumatoria);
+    // Pregunta 2-c
+    printf("Pregunta 2.c\n");
+    m = (int)powf(10.0, 8.0);
+    for (int block_pow=6; block_pow<=9; block_pow++) {
+        // Preparar variables
+        t_i = 0.0;
+        block_size = (int)powf(2.0, (float)block_pow);
+        grid_size = (int)ceil((float)(m)/block_size);
+        init(y_ji, dev_y_ji, m);
+        
+        // Evaluacion
+        cudaEvent_t ct1, ct2;
+        cudaEventCreate(&ct1);
+        cudaEventCreate(&ct2);
+        cudaEventRecord(ct1);
+        for (int i=0; i<=n; ++i) {
+            update_paralelo<<<grid_size, block_size>>>(dev_y_ji, dt, t_i, m);
+            t_i += dt;
+        }
+        cudaEventRecord(ct2);
+        cudaEventSynchronize(ct2);
+        cudaEventElapsedTime(&elapsed, ct1, ct2);
+        cudaMemcpy(y_ji, dev_y_ji, (m)*sizeof(float), cudaMemcpyDeviceToHost);
+
+        //Mostrar resultado 
+        printf("block_size=%d elapsed=%f [ms]\n", block_size, elapsed);
+        
+        // Liberar memoria
+        cudaFree(dev_y_ji);
+        free(y_ji);
+    }
     return 0;
 }
